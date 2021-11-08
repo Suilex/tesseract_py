69201fa8f2a7385c328fea91deeb2245c4f73775
Suilex <52204898+Suilex@users.noreply.github.com>
2021-11-08 11:41:06 -0800

    Update py_tesseract.py

--- py_tesseract.py
+++ py_tesseract.py
@@ -1,48 +1,43 @@
-import datetime
 import os
 
 import pytesseract
-#from PIL.Image import core as _imaging
-#from . import _imaging as core
 from PIL import Image
-
 from connect_db import add_to_db
+from pdf2image import convert_from_path
 
-tesseract_exe_location = r'D:\PythonMiniProjects\tesseract\Tesseract-OCR\tesseract.exe'
-input_files_location = r'D:\www\Apache24\htdocs\armd\armd\web\scans'
-
-
-class open_img:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.f = Image.open(file_path)
-
-    def __enter__(self):
-        return self.f
-
-    def __exit__(self, *args):
-        self.f.close()
-        os.remove(self.file_path)
+pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract-OCR\tesseract.exe'
+MAIN_FOLDER = r'D:\Scans'
 
 
-def convert_to_database(img):
-    if img.size < (500, 500):
-        print("Unreadable")
-        return
-    return pytesseract.image_to_string(img, lang="rus")
+def tesseract_extract_img(file, deal_id, user_id):
+    img = Image.open(file)
+    data = pytesseract.image_to_string(img, lang='rus')
+    add_to_db(deal_id, user_id, data)
 
 
-def to_path(*args):
-    return "\\".join(args)
+def tesseract_extract_pdf(file, deal_id, user_id):
+    data = ""
+    for img in convert_from_path(file):
+        data += pytesseract.image_to_string(img, lang='rus')
+    add_to_db(deal_id, user_id, data)
 
 
-def to_start():
-    log = ''
-    for directory in os.listdir(path=input_files_location):
-        for file in os.listdir(path=to_path(input_files_location, directory)):
-            with open_img(to_path(input_files_location, directory, file)) as img:
-                deal_id, user_id, _ = directory.split('_')
-                log += 'day: {}   deal_id: {}   user_id: {}\n'.format(str(datetime.datetime.now()), deal_id, user_id)
-                add_to_db(deal_id, user_id, convert_to_database(img))
-        os.rmdir(path=to_path(input_files_location, directory))
-    return log
+def main():
+    for folder in os.listdir(MAIN_FOLDER):
+        deal_id, user_id = folder.split('_')[:2]
+        path_of_folder = os.path.join(MAIN_FOLDER, folder)
+        for file in os.listdir(path_of_folder):
+            path_of_file = os.path.join(path_of_folder, file)
+            extension = file.split('.')[-1].lower()
+            if extension == 'jpg' or extension == 'jpeg':
+                tesseract_extract_img(path_of_file, deal_id, user_id)
+            elif extension == 'pdf':
+                tesseract_extract_pdf(path_of_file, deal_id, user_id)
+            else:
+                pass
+            os.remove(path_of_file)
+        os.rmdir(path_of_folder)
+
+
+if __name__ == '__main__':
+    main()
