e1264bd7c1fc61256cdf3c334039fcd71488f78f
Suilex <52204898+Suilex@users.noreply.github.com>
2021-11-23 19:00:53 -0800

    Update indictment

--- connect_db.py
+++ connect_db.py
@@ -4,8 +4,8 @@ import pymysql.cursors
 def to_connect():
     return pymysql.connect(host='localhost',
                            user='root',
-                           password='',
-                           db='oodt',
+                           password='root',
+                           db='ood',
                            charset='utf8',
                            cursorclass=pymysql.cursors.DictCursor)
 
@@ -18,7 +18,7 @@ def to_close(connection):
     connection.close()
 
 
-def add_to_db(deal_id, user_id, text):
+def add_to_db(deal_id, user_id, text, indictment):
     # В sql запросе используется одинарная кавычка, поэтому избавляемся от неё в тексте
     if text.find("'") != -1:
         text = '"'.join(text.split("'"))
@@ -27,10 +27,24 @@ def add_to_db(deal_id, user_id, text):
     try:
         with connection.cursor() as cursor:
 
-            sql = "INSERT INTO scans (deal_id, user_id, text) VALUES ({}, {}, '{}')" \
-                .format(deal_id, user_id, text)
+            if indictment:
+                check_row_sql = "SELECT * FROM indictment WHERE deal_id = {}" \
+                    .format(deal_id)
+                execute = cursor.execute(check_row_sql)
+                if execute:
+                    update_sql = "UPDATE indictment SET evidences = '{}' WHERE deal_id = {}"\
+                        .format(text, deal_id)
+                    cursor.execute(update_sql)
+                else:
+                    insert_sql = "INSERT INTO indictment (deal_id, evidences, established, justification) VALUES ({}, '{}', ' ', ' ')"\
+                        .format(deal_id, text)
+                    cursor.execute(insert_sql)
+            else:
 
-            cursor.execute(sql)
+                sql = "INSERT INTO scans (deal_id, user_id, text) VALUES ({}, {}, '{}')" \
+                    .format(deal_id, user_id, text)
+                cursor.execute(sql)
+
             to_commit(connection)
     finally:
         to_close(connection)
